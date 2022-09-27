e8cc91668ae7edb8df24c4c7bd4fecd7025c8573
x-lek <96938521+x-lek@users.noreply.github.com>
2022-09-28 00:42:18 +0800

    add maxio-api project template

---
+++ maxio-api/maxio-api/src/main/java/com/maxio/maxioapi/MaxioApiApplication.java
@@ -0,0 +1,13 @@
+package com.maxio.maxioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxioApiApplication.class, args);
+	}
+
+}
---
+++ maxio-api/maxio-api/src/test/java/com/maxio/maxioapi/MaxioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxio.maxioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
